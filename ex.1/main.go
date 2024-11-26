0be06fa5c10ea3c7311972a8b9fde4735631ea7c
Ming <school83025@gmail.com>
2024-11-26 18:48:34 +0800

    ex01

---
+++ ex.1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var a int = 10
+
+const b string = "Hello World"
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	c := 20
+	fmt.Printf("The value of c is %d\n", c)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/b0742020/Golang_Excercise
+
+go 1.23.1
